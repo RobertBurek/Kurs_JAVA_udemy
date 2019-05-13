2305198126f42ba75785755ddec3c7dc04fe6114
Robert Burek <robertburek@wp.pl>
2019-05-13 16:29:38 +0200

    Animacja Kropli  - wiele wątków, możliwość zatrzymania i skasowania

--- AnimacjaKropli/src/Main.java
+++ AnimacjaKropli/src/Main.java
@@ -8,15 +8,33 @@ import java.util.ArrayList;
  * Created by Robert Burek
  */
 public class Main extends JFrame {
+
+    public static Boolean isPauza = false;
+
     public Main() {
         this.setTitle("Animacja kropelki");
-        this.setBounds(50, 100, 700, 550);
+        this.setBounds(250, 300, 300, 250);
         panelAnimacji.setBackground(Color.GRAY);
         JButton bStart = (JButton) panelButtonow.add(new JButton("Start"));
-
         bStart.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 startAnimation();
+            }
+        });
+
+        JButton bUsun = (JButton) panelButtonow.add(new JButton("Usuń"));
+        bUsun.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                stopAnimation();
+            }
+        });
+
+        JButton bPauza = (JButton) panelButtonow.add(new JButton("Pauza"));
+        bPauza.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (!isPauza) bPauza.setText("Wznów");
+                else bPauza.setText("Pauza");
+                pauzaAnimation();
             }
         });
 
@@ -25,8 +43,16 @@ public class Main extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    private void pauzaAnimation() {
+        panelAnimacji.pauza();
+    }
+
     public void startAnimation() {
         panelAnimacji.addKropelka();
+    }
+
+    public void stopAnimation() {
+        panelAnimacji.stop();
     }
 
     private JPanel panelButtonow = new JPanel();
@@ -40,33 +66,73 @@ public class Main extends JFrame {
 
         public void addKropelka() {
             listaKropelek.add(new Kropelka());
-            for (int i = 0; i < 2500; i++) {
-                for (int j = 0; j < listaKropelek.size(); j++) {
-                    ((Kropelka) listaKropelek.get(j)).ruszKropelka(this);
-                    this.paint(this.getGraphics());
-                    try {
-                        Thread.sleep(1);
-                    } catch (InterruptedException ex) {
-                        System.out.println(ex.getMessage());
-                    }
-                }
-            }
+            watek = new Thread(grupaWatkow, new KropelkaRunnable((Kropelka) listaKropelek.get(listaKropelek.size() - 1)));
+            watek.start();
+            grupaWatkow.list();
+        }
+
+        public void stop() {
+            grupaWatkow.interrupt();
+        }
+
+        public void pauza() {
+            if (Main.isPauza) Main.isPauza = false;
+            else Main.isPauza = true;
+            System.out.println("Pauza -" + Main.isPauza.toString());
         }
 
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-
             for (int i = 0; i < listaKropelek.size(); i++) {
                 g.drawImage(Kropelka.getImg(), ((Kropelka) listaKropelek.get(i)).x, ((Kropelka) listaKropelek.get(i)).y, null);
             }
         }
 
         ArrayList listaKropelek = new ArrayList();
+        JPanel ten = this;
+        Thread watek;
+        ThreadGroup grupaWatkow = new ThreadGroup("Grupa Kropelek");
+
+
+        public class KropelkaRunnable implements Runnable {
+
+            Kropelka kropelka;
+
+            public KropelkaRunnable(Kropelka kropelka) {
+                this.kropelka = kropelka;
+            }
+
+            public void run() {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        if (!Main.isPauza) {
+                            this.kropelka.ruszKropelka(ten);
+                            repaint();
+                            Thread.sleep(1);
+                        }
+                    }
+                } catch (InterruptedException ex) {
+                    System.out.println(ex.getMessage());
+                    listaKropelek.clear();
+                    repaint();
+                }
+            }
+        }
     }
 }
 
 class Kropelka {
+
+    public static Image kropelka = new ImageIcon("kropelka.gif").getImage();
+
+    int x = 0;
+    int y = 0;
+    int dx = 1;
+    int dy = 1;
+    int xKropelki = kropelka.getWidth(null);
+    int yKropelki = kropelka.getHeight(null);
+
     public static Image getImg() {
         return Kropelka.kropelka;
     }
@@ -75,7 +141,6 @@ class Kropelka {
         Rectangle granicePanelu = pojemnik.getBounds();
         x += dx;
         y += dy;
-
         if (y + yKropelki >= granicePanelu.getMaxY()) {
             y = (int) (granicePanelu.getMaxY() - yKropelki);
             dy = -dy;
@@ -88,20 +153,9 @@ class Kropelka {
             y = (int) granicePanelu.getMinY();
             dy = -dy;
         }
-
         if (x < granicePanelu.getMinX()) {
             x = (int) granicePanelu.getMinX();
             dx = -dx;
         }
-
     }
-
-    public static Image kropelka = new ImageIcon("kropelka.gif").getImage();
-
-    int x = 0;
-    int y = 0;
-    int dx = 1;
-    int dy = 1;
-    int xKropelki = kropelka.getWidth(null);
-    int yKropelki = kropelka.getHeight(null);
 }
